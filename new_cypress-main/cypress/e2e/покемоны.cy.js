499f3069a4daee7db04019ecc2a48a988f5c3f58
vkondratt <vladkondratkov1@gmail.com>
2025-06-05 17:46:37 +0300

    Add files via upload

---
+++ new_cypress-main/cypress/e2e/покемоны.cy.js
@@ -0,0 +1,21 @@
+describe('Проверка покупки нового аватара', function () {                 
+    it('e2e тест на покупку нового аватара для тренера', function () {   
+         cy.visit('https://pokemonbattle.ru/');                         
+         cy.get('input[id="k_email"]').type('kondratkov.vv@yandex.ru');                 
+         cy.get('input[id="k_password"]').type('Vlad24128251');             
+         cy.get('button[type="submit"]').click();                
+         cy.wait(2000);
+         cy.get('.header_card_trainer').click();           
+         cy.wait(2000);
+         cy.get('.k_mobile > :nth-child(5) > #dropdown > img').click(); 
+         cy.get('.available > button').first().click();   
+         cy.get('.card_number').type('4620869113632996');                    
+         cy.get('.card_csv').type('125');                            
+         cy.get('.card_date').type('1226');                          
+         cy.get('.card_name').type('NAME');                          
+         cy.get('.style_1_base_button_payment_body > .style_1_base_button_payment').click();    
+         cy.get('.threeds_number').type('56456');                           
+         cy.get('.style_1_base_button_payment_body > .style_1_base_button_payment').click();  
+         cy.contains('Покупка прошла успешно').should('be.visible');    
+     });
+ });
